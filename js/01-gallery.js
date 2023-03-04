ef0e58f1d9b241e35e70ea76651e7d8243137f05
Kriminz <artur26062004@gmail.com>
2023-03-04 19:34:07 +0200

    Перше завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const createGallery = createGalleryItems(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", createGallery);
+
+// Створення розмітки
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+      </div>
+    `;
+    })
+    .join("");
+}
+
+gallery.addEventListener("click", checkCurrentImage);
+
+function checkCurrentImage(event) {
+// Заборона завантаження картинки
+  event.preventDefault();
+
+// Перевірка місця кліку
+  checkClickPlase(event);
+
+// Створення модального вікна з більшим зображенням
+  createModal(event);
+}
+
+function checkClickPlase(event) {
+  const isImage = event.target.classList.contains("gallery__image");
+
+  if (!isImage) {
+    return;
+  }
+}
+
+function createModal(event) {
+  const instance = basicLightbox.create(`
+  <img src="${event.target.dataset.source}" width="800" height="600">`);
+
+instance.show();
+
+gallery.addEventListener('keydown', (e) => {
+  if(e.code === "Escape"){
+    instance.close();
+  }
+});
+}
